b541150f8cdae31630424d2374112cd4cde35963
mkreynolds919 <mkreynolds919@gmail.com>
2025-03-07 11:51:09 -0600

    Create todo.js file
    Create constructor for Todo class

---
+++ src/todo.js
@@ -0,0 +1,11 @@
+
+class Todo {
+    constructor(title, description, dueDate, priority, notes, status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes;
+        this.status = status;
+    }
+}
