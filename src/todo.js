09bb87332cc46a4258e4a4247f655720e4818752
mkreynolds919 <mkreynolds919@gmail.com>
2025-03-07 12:24:42 -0600

    Change priority, dueDate, and status to internal variables
    export todo class to index.js

--- src/todo.js
+++ src/todo.js
@@ -1,25 +1,25 @@
 
-class Todo {
+export default class Todo {
     constructor(title, description, dueDate, priority, notes, status) {
         this.title = title;
         this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
+        this._dueDate = dueDate;
+        this._priority = priority;
         this.notes = notes;
-        this.status = status;
+        this._status = status;
     }
 
     set priority(value) {
         const priorities = ["low", "medium", "high"];
         if (priorities.includes(value)) {
-            this.priority = value;
+            this._priority = value;
         }
     }
 
     set status(value) {
         const statuses = ["not started", "in progress", "completed"];
         if (statuses.includes(value)) {
-            this.status = value;
+            this._status = value;
         }
     }
 }
